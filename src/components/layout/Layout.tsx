c13957f96a483afa0368935cc352760cbdb21981
Przestrzel <marek.przestrzelski00@gmail.com>
2023-10-25 22:08:19 +0200

    Add toasts for notifications

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from "react-hot-toast";
 import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
     <main className="w-screen h-screen bg-gray-200 px-24">
       <Outlet />
+      <Toaster position="bottom-left" reverseOrder={false} />
     </main>
   );
 }
