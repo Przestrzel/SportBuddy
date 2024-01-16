b5587028b6d3e0e3bbfab61b1893353341935ce9
Przestrzel <marek.przestrzelski00@gmail.com>
2024-01-16 21:55:24 +0100

    Refactor places of files

--- src/store/services/auth.services.ts
+++ src/store/services/auth.services.ts
@@ -1,5 +1,5 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
-import { baseQuery } from "./utils.services";
+import { baseQuery } from "../utils/services.utils";
 
 export const authApi = createApi({
   baseQuery,
